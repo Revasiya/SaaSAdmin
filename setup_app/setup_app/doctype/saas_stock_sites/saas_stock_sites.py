2b34036987b97bb992ea3383f8d3504740f0a524
rohit.kumar-at-942428920331 <59276987+RohitKumarGit@users.noreply.github.com>
2023-05-09 16:46:05 +0000

    done

--- setup_app/setup_app/doctype/saas_stock_sites/saas_stock_sites.py
+++ setup_app/setup_app/doctype/saas_stock_sites/saas_stock_sites.py
@@ -1,12 +1,34 @@
 # Copyright (c) 2023, OneHash and contributors
 # For license information, please see license.txt
-
+domain = "localhost"
 import frappe
 from frappe.model.document import Document
-
+import os
+from frappe.utils import random_string
 def getSiteConfig():
     siteConfig = frappe.get_doc("SaaS settings")
     return siteConfig
+def executeCommands(commands):
+    command = " & ".join(commands)
+    os.system(command)
+def refreshStockSites():
+    # this function runs every day and maintains the stock site
+    config = getSiteConfig()
+    commands = []
+    currentStock = frappe.db.get_list("SaaS stock sites",filters={"isUsed":"no"})
+    print("In stock",len(currentStock))
+    if len(currentStock) < int(config.stock_site_count):
+        number_of_sites_to_stock = int(config.stock_site_count) - len(currentStock)
+        for _ in range(number_of_sites_to_stock):
+            subdomain = random_string(10)
+            adminPassword = random_string(5)
+            print("creating",subdomain)
+            commands.append("bench new-site {} --install-app erpnext --admin-password {} --db-root-password {}".format(subdomain + '.'+domain,adminPassword,config.db_password))
+            site = frappe.new_doc("SaaS stock sites")
+            site.subdomain = subdomain
+            site.admin_password = adminPassword
+            site.insert()
+    executeCommands(commands)
 class SaaSstocksites(Document):
 	pass
         
--- setup_app/setup_app/doctype/saas_stock_sites/test_saas_stock_sites.py
+++ setup_app/setup_app/doctype/saas_stock_sites/test_saas_stock_sites.py
@@ -5,11 +5,27 @@ import frappe
 from frappe.tests.utils import FrappeTestCase
 import unittest
 from .saas_stock_sites import getSiteConfig
-
+from .saas_stock_sites import refreshStockSites
+import random
+def deleteSomeSites():
+    config = getSiteConfig()
+    td = random.randint(1,int(config.stock_site_count))
+    sites = frappe.db.get_list("SaaS stock sites",filters={"isUsed":"no"})
+    if(len(sites) < int(config.stock_site_count)):
+        return
+    print("deleting ",td,"sites for testing")
+    for i in range(td):
+        name = sites[i]['name']
+        frappe.delete_doc("SaaS stock sites",name)
+     
 class TestSaaSstocksites(unittest.TestCase):
     def test_getting_site_config(self):
         config = getSiteConfig()
-        print(config)
-        self.assertEqual(config["stock site count"],15)
-        
+        self.assertIsInstance(config.stock_site_count,str)
+    def test_manage_stock_sites(self):
+        config = getSiteConfig()
+        deleteSomeSites()
+        refreshStockSites()
+        self.assertEqual(len(frappe.db.get_list("SaaS stock sites",filters={"isUsed":"no"})),int(config.stock_site_count))
+        pass
         
