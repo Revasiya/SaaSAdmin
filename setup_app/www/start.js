b2fe1805c8292527cf2b5c07480d79fcd114a284
rohit.kumar-at-942428920331 <59276987+RohitKumarGit@users.noreply.github.com>
2023-05-25 13:10:16 +0530

    done

--- setup_app/www/start.js
+++ setup_app/www/start.js
@@ -17,18 +17,24 @@ createApp({
       lname: "",
       email: "",
       password: "",
+      loading: false,
       sitename: "",
       comapany_name: "",
       inputErrors: [],
+      status: {
+        step1: "neutral",
+        step2: "neutral",
+        step3: "neutral",
+      },
     };
   },
+
   mounted() {
     inputClasses = document.querySelectorAll(".input");
     console.log(inputClasses);
     inputClasses.forEach((input) => {
       document.getElementById(input.id).addEventListener("focus", () => {
-        console.log("focus");
-        input.classList.remove("error");
+        input.classList.remove("uk-form-danger");
       });
       document.getElementById(input.id).addEventListener("onchange", () => {
         console.log("change");
@@ -39,6 +45,7 @@ createApp({
     frappe.socketio.init();
 
     frappe.socketio.socket.on("site_created", (data) => {
+      this.status.step3 = "completed";
       if (window.dev_server) {
         window.location.href = `http://${data.site}.${domain}:8000`;
       } else {
@@ -52,40 +59,42 @@ createApp({
   methods: {
     validateInput(fname, lname, email, password, sitename, comapany_name) {
       this.inputErrors = [];
+      const errorClass = "uk-form-danger";
       let isOk = true;
       if (!fname) {
         this.inputErrors.push("First Name is required");
-        document.getElementById("firstname").classList.add("error");
+        document.getElementById("firstname").classList.add(errorClass);
         isOk = false;
       }
       if (!lname) {
         this.inputErrors.push("Last Name is required");
-        document.getElementById("lastname").classList.add("error");
+        document.getElementById("lastname").classList.add(errorClass);
         isOk = false;
       }
       if (!email) {
         this.inputErrors.push("Email is required");
-        document.getElementById("email").classList.add("error");
+        document.getElementById("email").classList.add(errorClass);
         isOk = false;
       }
       if (!password) {
         this.inputErrors.push("Password is required");
-        document.getElementById("password").classList.add("error");
+        document.getElementById("password").classList.add(errorClass);
         isOk = false;
       }
       if (!sitename) {
         this.inputErrors.push("Site Name is required");
-        document.getElementById("sitename").classList.add("error");
+        document.getElementById("sitename").classList.add(errorClass);
         isOk = false;
       }
       if (!comapany_name) {
         this.inputErrors.push("Company Name is required");
-        document.getElementById("companyname").classList.add("error");
+        document.getElementById("companyname").classList.add(errorClass);
         isOk = false;
       }
       return isOk;
     },
     createSite() {
+      this.loading = true;
       console.log(
         "crete site",
         this.fname,
@@ -105,8 +114,11 @@ createApp({
           this.comapany_name
         )
       ) {
+        this.loading = false;
         return;
       }
+      this.status.step1 = "loading";
+
       frappe.call({
         method: "setup_app.setup_app.doctype.saas_sites.saas_sites.setupSite",
         args: {
@@ -119,7 +131,9 @@ createApp({
             last_name: this.lname,
           },
         },
-        callback: function (r) {
+        callback: (r) => {
+          this.status.step1 = "completed";
+          this.status.step2 = "completed";
           if (r.message) {
             console.log(r.message);
           }
